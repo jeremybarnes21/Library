9d2174734f954446ccd234f4f906d248859a9b0c
Jeremy Barnes <jeremypb21@gmail.com>
2022-02-18 13:11:43 -0500

    Added DOM functions and forEach that loops through the Library Array and displays books. Need to add function that automatically adds books to array when new book is created

--- app.js
+++ app.js
@@ -1,4 +1,64 @@
-const para = document.querySelector('#test');
+const myLibrary = [];
+function Book(title, author, pages){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+}
+const knowingGod = new Book('Knowing God','J.I Packer','278');
+const mereChristianity = new Book('Mere Christianity','C.S Lewis','177');
+const pilgrimsProgress = new Book(`The Pilgrim's Progress`,'John Bunyan','304');
+
+
+function addBookToLibrary(book){
+    myLibrary.push(book);
+}
+addBookToLibrary(knowingGod);
+addBookToLibrary(mereChristianity);
+addBookToLibrary(pilgrimsProgress);
+
+console.log(myLibrary);
+console.log(knowingGod.title);
+
+//functions to create DOM for each book... will be used by forEach function
+const cardContainer = document.querySelector('#cardContainer');
+let cardDiv;
+let titleHeader;
+let authorInfo;
+let pagesInfo;
+function createCardDiv(){
+    cardDiv = document.createElement('div');
+    cardDiv.className = 'card';
+    cardContainer.appendChild(cardDiv);
+}
+function createTitleHeader(title){
+    titleHeader = document.createElement('h4');
+    titleHeader.innerHTML = title;
+    titleHeader.className = 'bookTitle';
+    cardDiv.appendChild(titleHeader);
+}
+function createAuthorPara(author){
+    authorInfo = document.createElement('p');
+    authorInfo.innerHTML = author;
+    authorInfo.className = 'author';
+    cardDiv.appendChild(authorInfo);
+}
+function createPagesPara(pages){
+    pagesInfo = document.createElement('p');
+    pagesInfo.innerHTML = pages;
+    pagesInfo.className = 'pages';
+    cardDiv.appendChild(pagesInfo);
+}
+//function to loop through array and display
+myLibrary.forEach((book)=>{
+    createCardDiv();
+    createTitleHeader(book.title);
+    createAuthorPara(book.author);
+    createPagesPara(book.pages);
+
+
+});
+
+/*const para = document.querySelector('#test');
 const aCBtn = document.querySelector('#aC');
 const mCBtn = document.querySelector('#mC');
 
@@ -29,4 +89,4 @@ const mereChristianity = new book('Mere Christianity','C.S Lewis','215',yesRead)
 const abbasChild = new book(`Abba's Child`,'Brennan Manning','175',yesRead);
 
 aCBtn.addEventListener('click',pickAC);
-mCBtn.addEventListener('click',pickMC);
+mCBtn.addEventListener('click',pickMC);*/
